c52949f2b56d44af3476e97da1e6553786f02e7a
nmearl <contact@nicholasearl.me>
2018-01-21 14:20:08 -0500

    Use new electron-vue templates

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="section">
+      <div class="columns">
+        <div id="sidebar" class="column is-3 is-fullheight">
+          <div class="container">
+            <p>this is a test</p>
+          </div>
+        </div>
+        <div id="tab-area" class="column is-fullheight">
+          <div class="container">
+            <p>this is a test</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+#sidebar {
+  background-color: aqua;
+}
+
+#tab-area {
+  background-color: blanchedalmond;
+}
+</style>
