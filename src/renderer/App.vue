b71e1adace7677dce1f891cb327eea856cf2efb6
Nicholas Earl <contact@nicholasearl.me>
2018-01-22 10:14:53 -0500

    Initial component creation

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,43 @@
 <template>
   <div id="app">
-    <landing-page></landing-page>
+    <div class="columns">
+      <div id="tool-bar" class="column">
+        <tool-bar></tool-bar>
+      </div>
+    </div>
+    <div class="columns">
+      <div id="side-bar" class="column">
+
+      </div>
+      <div id="tab-area" class="column">
+        <tab-area></tab-area>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import LandingPage from '@/components/LandingPage'
+  import TabArea from '@/components/TabArea'
+  import ToolBar from '@/components/ToolBar'
 
   export default {
     name: 'vue-client',
     components: {
-      LandingPage
+      LandingPage,
+      TabArea,
+      ToolBar
     }
   }
 </script>
 
 <style>
-  /* CSS */
+#side-bar {
+  background-color: aqua;
+  height: calc(100vh - 50px);
+}
+
+#tab-area {
+  height: calc(100vh - 50px);
+}
 </style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import axios from 'axios'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 
 import App from './App'
 import store from './store'
+
+Vue.use(Buefy)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
